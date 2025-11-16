e179c21788ac1dc089e347a254961b170dba9dc8
Ft.DevOps <adityakhadkikar6@gmail.com>
2025-11-16 10:46:14 +0530

    chore: initial project scaffold - create django project & blog app, add Post model + initial migration

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blog Website - v1 (Initial Stage)
+
+This is v1.0.0 - Initial Django project scaffold and Post model
+
+What's included:
+- Django project created
+- blog app added
+- Post model (title, content, author, date_posted)
+- initial migrations and sqlite DB
+
+How to run (local):
+1. python -m venv venv
+2. source venv/Scripts/activate
+3. pip install -r requirements.txt
+4. python manage.py migrate
+5. python manage.py runserver
---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
