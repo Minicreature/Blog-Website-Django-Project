c64b275932f32a45a4e7aefc7220358615a38d8d
Ft.DevOps <adityakhadkikar6@gmail.com>
2025-11-17 21:37:18 +0530

    feat: add home (post listing) and post detail views + templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'   # <app>/<template>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']        # newest first
+    paginate_by = 5                    # optional: 5 posts per page
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
